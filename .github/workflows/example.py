bbee70f57cd104ee5037aa74648888dcae80167b
Herbert Tiano <84073549+herbydt@users.noreply.github.com>
2025-05-28 15:35:05 +1200

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,5 +1,11 @@
+import os
+
 def main():
   print("Hello for Git actions")
-
+  token = os.environment.get("XRAY_ID")
+  if not token:
+    raise RuntimeError("Token Not Found!")
+  print(token)
+  
 if __name__ == '__main__':
   main()

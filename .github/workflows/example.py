de47c384e390468b37c5854cbe0150e4632a2ccc
herbydt <herbert.tiano@gmail.com>
2025-05-28 16:15:23 +1200

    Test Behave Run

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,17 +1,18 @@
 import os
 
-def main():
-  print("Hello for Git actions")
-  secret = os.environ.get("XRAY_SECRET")
-  if not secret:
-    raise RuntimeError("Token Not Found!")
-  print(secret)
 
-  token = os.environ.get("XRAY_ID")
-  if not token:
-    raise RuntimeError("Token Not Found!")
-  print(token)
+def main():
+    print("Hello for Git actions")
+    secret = os.environ.get("XRAY_SECRET")
+    if not secret:
+        raise RuntimeError("Token Not Found!")
+    print(secret)
+
+    token = os.environ.get("XRAY_ID")
+    if not token:
+        raise RuntimeError("Token Not Found!")
+    print(token)
 
 
 if __name__ == '__main__':
-  main()
+    main()
---
+++ features/tests/test_run.feature
@@ -0,0 +1,5 @@
+@behave @sample_tag
+Feature: JIRA/XRAY - Test Execution Reporting
+
+  Scenario: This is a sample scenario
+    Given this step is ran

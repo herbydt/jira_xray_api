7c4c95a2c265f3f2ae955564d2eec8af4267ab7f
herbydt <herbert.tiano@gmail.com>
2025-06-01 15:35:24 +1200

    Publish Artifacts

--- features/steps/base_steps.py
+++ features/steps/base_steps.py
@@ -1,6 +1,7 @@
-from behave import given, when, step
+from behave import given
 import pandas as pd
 import csv
+import openai
 
 
 @given('the initialize method is completed')
@@ -13,5 +14,12 @@ def step_impl(context):
     sample_data = [['ABC', 123]]
     cols = ['Letters', 'Numbers']
     df = pd.DataFrame(sample_data, columns=cols)
-    df.to_csv('output/outputs/sample_csv.csv', index=False)
+    df.to_csv('resources/test_run_results/sample_csv.csv', index=False)
     print("Successful Run!!!!")
+
+
+@given('this next step is ran')
+def step_impl(context):
+    with open('resources/test_run_results/sample_csv.csv', 'r') as content:
+        results = pd.read_csv(content)
+    print(results['Letters'][0])
